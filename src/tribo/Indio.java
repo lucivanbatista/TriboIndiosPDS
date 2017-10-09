2c1e64ca2ad6cca0f3ccbe22aa3ee424e231a29e
lucivanbatista <lucivanbatista98@gmail.com>
2017-10-09 14:11:37 -0300

    Questão Inicial - Sem modificações

---
+++ src/tribo/Indio.java
@@ -0,0 +1,14 @@
+package tribo;
+
+public class Indio {
+	private String nome; 
+	public Indio(String nome){ 
+		this.nome = nome; 
+	} 
+	public String getNome() { 
+		return nome; 
+	} 
+	public void setNome(String nome) { 
+		this.nome = nome; 
+	} 
+}
